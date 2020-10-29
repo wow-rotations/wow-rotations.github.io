828a2aebc20be39419fe2c7003cddbfa1fa7c9db
wow-rotations <nic4eva@gmail.com>
2020-10-29 20:28:21 +1300

    Added clips of rogue, dk, and dh

--- src/js/components/SpecPreview.js
+++ src/js/components/SpecPreview.js
@@ -7,8 +7,10 @@ export default class RotationPreview extends React.Component {
         return <div className={"SpecPreviewContainer"} style={{"width":this.props.width}}>
             <h3>{this.props.label}</h3>
             <video
+                muted={true}
                 src={this.props.src}
-                autoPlay={true} loop={true}>
+                autoPlay={true}
+                loop={true}>
                 Your browser does not support embedded video
                 through HTML5.
             </video>
